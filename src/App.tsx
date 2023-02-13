7ecaa7b6d51ad352e2264e594162e971d65900cb
patrickbasham <PatrickTBasham@gmail.com>
2023-02-13 15:18:11 -0500

    react creating components for each input field coming in. added some base styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react"
 import './assets/styling/App.css'
 
+import { dynamicInputsData } from "./data/inputData.js"
+
 function App() {
+
+    console.log(dynamicInputsData)
+
+// const csvFile = "./data/dynamic questions.csv"
+
 
 const [inputFields, setInputFields] = useState([])
 
   return (
     <div className="App">
-        <form action="">
-
+        <form className="form-wrapper">
+            {dynamicInputsData.map((current: object, idx: number) => (
+                <>
+                <label >{current.question}</label>
+                <input type="text" key={idx}  />
+                </>
+            ))}
         </form>
     </div>
   )
