7cabba06bdfb8ab3671483c9e9a520eb4b77a840
patrickbasham <PatrickTBasham@gmail.com>
2023-02-13 16:15:58 -0500

    set up base structure for form inputs and select tags

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,35 @@ function App() {
 
     console.log(dynamicInputsData)
 
-// const csvFile = "./data/dynamic questions.csv"
+    // const csvFile = "./data/dynamic questions.csv"
 
 
-const [inputFields, setInputFields] = useState([])
+    const [inputFields, setInputFields] = useState([])
 
-  return (
-    <div className="App">
-        <form className="form-wrapper">
-            {dynamicInputsData.map((current: object, idx: number) => (
-                <>
-                <label >{current.question}</label>
-                <input type="text" key={idx}  />
-                </>
-            ))}
-        </form>
-    </div>
-  )
+    return (
+        <div className="App">
+            <form className="form-wrapper">
+                {dynamicInputsData.map((current: object, idx: number) => (
+                    current.value_type === "dropdown" ?
+                        <div key={idx
+                        }>
+                            <h4>{current.question}</h4>
+                            <div><label htmlFor=""></label>yes<input type="radio" name="answer" id="yes" value="yes" /></div>
+                            <div><label htmlFor=""></label>no<input type="radio" name="answer" id="no" value="no" /></div>
+                            <div><label htmlFor=""></label>idk<input type="radio" name="answer" id="idk" value="idk" /></div>
+                        </div>
+                        :
+                        <div key={idx
+                        }>
+                            <label>{current.question}</label>
+                            <select>
+                                <option value="abc">abc</option>
+                            </select>
+                        </div>
+                ))}
+            </form>
+        </div>
+    )
 }
 
 export default App
